8cdcebe1cced39f2b692330043441ba19dd969ae
fc_arny <arthur.shcheglov@gmail.com>
2012-11-03 02:22:40 +0400

    Install Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sap::Application.config.secret_token = 'f44832319efa73dbd21ce06630cbabe2df144f8377bb454365b9566efc14380cf6e12c6367b4549d87a401d41eb3f2da02a70250cf7118b9acd1759ff1aa6db6'
