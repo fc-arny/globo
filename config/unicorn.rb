9d9c47e9f0cee2c673225952432b4bb154f255f9
fc_arny <arthur.shcheglov@gmail.com>
2012-11-03 12:02:12 +0400

    Install Unicorn server

---
+++ config/unicorn.rb
@@ -0,0 +1,23 @@
+worker_processes 2
+working_directory "/var/www/sap/"
+
+preload_app true
+
+timeout 30
+
+listen "/var/www/sap/tmp/sockets/unicorn.sock", :backlog => 64
+
+pid "/var/www/sap/tmp/pids/unicorn.pid"
+
+stderr_path "/var/www/sap/log/unicorn.stderr.log"
+stdout_path "/var/www/sap/log/unicorn.stdout.log"
+
+before_fork do |server, worker|
+	defined?(ActiveRecord::Base) and
+			ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+	defined?(ActiveRecord::Base) and
+			ActiveRecord::Base.establish_connection
+end
