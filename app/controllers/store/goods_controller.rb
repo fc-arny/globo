a727f81505a10e884c12a6e5939472c7488164d4
fc_arny <arthur.shcheglov@gmail.com>
2012-11-28 02:34:28 +0400

    sap-2 Add session table

--- app/controllers/store/goods_controller.rb
+++ app/controllers/store/goods_controller.rb
@@ -26,7 +26,7 @@ class Store::GoodsController < ApplicationController
 	# View product
 	# -------------------------------------------------------------
 	def product
-		exit
+
 	end
 
 	# -------------------------------------------------------------
--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,1 +1,2 @@
+#encoding: utf-8
 APP_DEFAULT_NAME = 'GloboMarket'
