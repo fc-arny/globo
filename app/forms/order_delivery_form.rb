9a43bcfe0d026bdd936f7515e61c11836c008e11
fc_arny <arthur.shcheglov@gmail.com>
2013-09-08 00:22:37 +0400

    Auth complete

--- app/forms/order_delivery_form.rb
+++
@@ -1,7 +0,0 @@
-# -------------------------------------------------------------
-# Delivery Form
-# -------------------------------------------------------------
-class OrderDeliveryForm < ActiveForm
-  # Fields
-  attr_accessor :address, :time
-end
