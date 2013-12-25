6aaa11e74fd4bcd19bd9f6b2daee615b19895112
fc_arny <arthur.shcheglov@gmail.com>
2013-12-25 00:32:44 +0000

    About page

--- app/controllers/common/static_controller.rb
+++ app/controllers/common/static_controller.rb
@@ -3,6 +3,8 @@
 # -------------------------------------------------------------
 class Common::StaticController < FrontendController
 
+  before_action :prepare_team
+
   def show
     render action: params[:page]
   rescue ActionView::MissingTemplate
@@ -18,5 +20,32 @@ class Common::StaticController < FrontendController
 
   def page_exists?
     %w(contacts help about promo).include? params[:page]
+  end
+
+  # Set team 
+  def prepare_team
+    @team = {
+      arthur: {
+        name: 'Артур Щеглов',
+        title: 'Основатель, СЕО и разработчик по совместительству'
+      },
+      sasha: {
+        name: 'Саша Трифан',
+        title: 'Дизайнер, творец и муж'
+      },
+      moroz: {
+        name: 'Артур Мороз',
+        title: 'Мастер фронтенда'
+      },
+      dima: {
+        name: 'Дима Хрешко',
+        title: 'Дизайнер UI'
+      },
+      alexey: {
+        name: 'Алексей Коробицын',
+        title: 'Java разработчик, архитектор'
+      },
+
+    }
   end
 end
