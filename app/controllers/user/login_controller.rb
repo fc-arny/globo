d2b038010a135f37957ace7aa2f19cfcc0e76bbe
fc_arny <arthur.shcheglov@gmail.com>
2014-05-28 02:51:15 +0400

    Some fixes, pluralization for rails_admin

--- app/controllers/user/login_controller.rb
+++ app/controllers/user/login_controller.rb
@@ -1,14 +1,12 @@
 # All login actions
 class User::LoginController < FrontendController
 
-  layout 'application_with_basket'
-
   # Login form
   # GET /login
   def form
     @form = Sap::AuthForm.new
     if request.xhr?
-      render :partial => 'partials/popups/welcome'
+      render partial: 'partials/popups/welcome'
     end
   end
 end
