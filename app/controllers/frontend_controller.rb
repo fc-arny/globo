3a338d73fd4001cb88a22dd29768cc7fce14b99b
fc_arny <arthur.shcheglov@gmail.com>
2014-06-21 15:51:13 +0400

    GM: Categories tree, css Sap: Fix API request to good_items

--- app/controllers/frontend_controller.rb
+++ app/controllers/frontend_controller.rb
@@ -8,37 +8,35 @@ class FrontendController < ApplicationController
   private
 
   # -------------------------------------------------------------
-  # Fetch stores, categories, order, order items and
-  # other needle data
+  # Fetch categories, order, order items and other needle data
   # -------------------------------------------------------------
   def bootstrap
-    # TODO: Cache it!
     # Set up current order
     @order = session[:order_id] ? Sap::Order.find(session[:order_id]) : nil
 
     ## Data client
     @stores = Sap::Store.active
-    @categories = gon.categories = Rails.cache.fetch('categories') { categoty_tree }
+    @categories = gon.categories = category_tree
   end
 
-  # Fetch categories
-  def categoty_tree
-    all = {}
-    roots = Sap::Category.menu.roots
-    roots.each do |item|
-      all[item.id] = {id: item.id, url: item.url, name: item.name, parent_id: nil }
+  # Fetch all categories
+  def category_tree
+    Rails.cache.fetch([:menu, :category, :tree], expires_in: 1.day) do
+      all = {}
+      roots = Sap::Category.menu.roots
+      roots.each do |item|
+        all[item.id] = {id: item.id, url: item.url, name: item.name, parent_id: nil }
 
-      item.children.each do |child|
-        next unless child.show_in_menu
-        children = child.children.map do |i|
-          {id: i.id, url: i.url, name: i.name, parent_id: i.id }
+        item.children.menu.each do |child|
+          children = child.children.menu.map do |i|
+            {id: i.id, url: i.url, name: i.name, parent_id: i.id }
+          end
+          all[child.id] = {id: child.id, url: child.url, name: child.name, parent_id: item.id, children: children }
         end
-        all[child.id] = {id: child.id, url: child.url, name: child.name, parent_id: item.id, children: children }
       end
+
+      all
     end
-
-    all
   end
-
 end
 
